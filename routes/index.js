0ba0eaf2f61c9248999507974ecc72dbe303092b
Lmint <jeonkyosin@gmail.com>
2019-01-29 14:34:26 +0700

    Fix IndexController

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { 
-  onAdd, 
+const {
+  onAdd,
   onLoad,
   onSearch,
   onAddLang,
@@ -10,26 +10,26 @@ const {
   handleEditContent,
   onDeleteContent,
   ShowAllLang,
-  DeleteLang,
-} = require('../controller/IndexController');
+  DeleteLang
+} = require("../controller/indexController");
 
 /* GET home page. */
-router.get('/', onLoad);
+router.get("/", onLoad);
 
-router.post('/add', onAdd);
+router.post("/add", onAdd);
 
-router.get('/search', onSearch);
+router.get("/search", onSearch);
 
-router.post('/add-lang', onAddLang);
+router.post("/add-lang", onAddLang);
 
-router.get('/country-code', ShowAllLang);
+router.get("/country-code", ShowAllLang);
 
-router.get('/edit-content/:engId/:langId/:langCode', onEditContent);
+router.get("/edit-content/:engId/:langId/:langCode", onEditContent);
 
-router.post('/update-content/:engId/:langId', handleEditContent);
+router.post("/update-content/:engId/:langId", handleEditContent);
 
-router.get('/delete-content/:engId/:langId/:uid', onDeleteContent);
+router.get("/delete-content/:engId/:langId/:uid", onDeleteContent);
 
-router.get('/delete-lang/:langId', DeleteLang);
+router.get("/delete-lang/:langId", DeleteLang);
 
 module.exports = router;
